1dcb94132f186f110095f738be752784c1ce4351
pegasys <sconnolly7@outlook.com>
2023-02-02 23:09:36 +0000

    Initial Commit

---
+++ ChivalryEngine.cs
@@ -0,0 +1,46 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace ChivalryEngineCore
+{
+    public abstract class ChivalryEngine
+    {
+        private Vector2 windowSize = new Vector2(512, 512);
+        private string windowTitle = "New Game";
+        private RenderWindow? window = null;
+        private Thread? gameLoop = null;
+
+        public Color BackgroundColour = Color.White;
+
+        public ChivalryEngine(Vector2 windowSize, string windowTitle)
+        {
+            this.windowSize = windowSize;
+            this.windowTitle = windowTitle;
+
+            gameLoop = new Thread(GameLoop);
+            gameLoop.Start();
+        }
+
+        private void GameLoop()
+        {
+            OnLoad();
+
+            window = new RenderWindow(new VideoMode((uint)windowSize.X, (uint)windowSize.Y), windowTitle);
+            window.Closed += (sender, e) => window.Close(); // Closes window when x is pressed
+
+            while (window.IsOpen)
+            {
+                OnUpdate();
+
+                window.DispatchEvents();
+                window.Clear(BackgroundColour);
+                window.Display();
+
+                Thread.Sleep((int)(1f/5f * 1000));
+            }
+        }
+
+        public abstract void OnLoad();
+        public abstract void OnUpdate();
+    }
+}
---
+++ Vector2.cs
@@ -0,0 +1,32 @@
+﻿namespace ChivalryEngineCore
+{
+    public class Vector2
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Vector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Vector2()
+        {
+            X = Zero.X;
+            Y = Zero.Y;
+        }
+        public static Vector2 Zero
+        { get { return new Vector2(0, 0); } }
+
+        public Vector2 Normalized
+        {
+            get
+            { 
+                float magnitude = (float)Math.Sqrt(X * X + Y * Y);
+                return new Vector2(X / magnitude, Y / magnitude);
+            }
+        }
+
+    }
+}
