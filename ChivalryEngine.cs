903a08a0ac27f90e339766a0c78770be960856e1
pegasys <sconnolly7@outlook.com>
2023-02-03 19:30:24 +0000

    Altered ChivalryEngine.cs to use Time to create a consistent logic frame rate separate from framerate

--- ChivalryEngine.cs
+++ ChivalryEngine.cs
@@ -1,4 +1,6 @@
-﻿using SFML.Graphics;
+﻿using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
 using SFML.Window;
 
 namespace ChivalryEngineCore
@@ -7,20 +9,29 @@ namespace ChivalryEngineCore
     {
         private Vector2 windowSize = new Vector2(512, 512);
         private string windowTitle = "New Game";
-        private RenderWindow? window = null;
-        private Thread? gameLoop = null;
 
-        public Color BackgroundColour = Color.White;
+        private RenderWindow window = new RenderWindow(new VideoMode(512, 512), "New Game");
+        private Thread gameLoop;
+        private Time gameTime;
+
+
+        public double LogicFrameTime { get; private set; } = 0;
+        public double DrawFrameTime { get { return gameTime.DeltaTime; } }
+        public double GameTimer { get { return gameTime.TotalTimeElapsed; } }
+        
+        public Color BackgroundColour { get; set; } = Color.White;
 
         public ChivalryEngine(Vector2 windowSize, string windowTitle)
         {
             this.windowSize = windowSize;
             this.windowTitle = windowTitle;
 
+            gameTime = new Time();
             gameLoop = new Thread(GameLoop);
             gameLoop.Start();
         }
 
+        private const float LOGIC_FRAMES_PER_SECOND = 50f;
         private void GameLoop()
         {
             OnLoad();
@@ -28,19 +39,31 @@ namespace ChivalryEngineCore
             window = new RenderWindow(new VideoMode((uint)windowSize.X, (uint)windowSize.Y), windowTitle);
             window.Closed += (sender, e) => window.Close(); // Closes window when x is pressed
 
+            double timeUntilNextFrame = 0f;
+            double timeOfLastFrame = 0f;
             while (window.IsOpen)
             {
-                OnUpdate();
-
                 window.DispatchEvents();
-                window.Clear(BackgroundColour);
-                window.Display();
 
-                Thread.Sleep((int)(1f/5f * 1000));
+                gameTime.Update();
+                timeUntilNextFrame -= gameTime.DeltaTime;
+                while (timeUntilNextFrame <= 0)
+                {
+                    timeUntilNextFrame = 1f / LOGIC_FRAMES_PER_SECOND * 1000;
+                    LogicFrameTime = gameTime.TotalTimeElapsed - timeOfLastFrame;
+                    timeOfLastFrame = gameTime.TotalTimeElapsed;
+
+                    OnUpdate();
+                }
+
+                window.Clear(BackgroundColour);
+                OnDraw();
+                window.Display();
             }
         }
 
         public abstract void OnLoad();
         public abstract void OnUpdate();
+        public abstract void OnDraw();
     }
 }
